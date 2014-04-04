a8e6aef48f2e55213d8734968e6ef27ea36b5865
Yushun He <sonnyhe2002@gmail.com>
2014-04-04 01:03:13 -0400

    Switch mail to mandrill

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,10 +1,10 @@
 if Rails.env.production?
   ActionMailer::Base.smtp_settings = {
-    port:           ENV['MAILGUN_SMTP_PORT'],
-    address:        ENV['MAILGUN_SMTP_SERVER'],
-    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
-    password:       ENV['MAILGUN_SMTP_PASSWORD'],
-    domain:         'unidler.herokuapp.com',
+    port:           '587'
+    address:        'smtp.mandrillapp.com',
+    user_name:      ENV['MANDRILL_USERNAME'],
+    password:       ENV['MANDRILL_APIKEY'],
+    domain:         'heroku.com',
     authentication: :plain,
   }
 
