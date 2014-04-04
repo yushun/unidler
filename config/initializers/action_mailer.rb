b5df64b077db32a75a09a3b99f45bd2da3cd0731
Yushun He <sonnyhe2002@gmail.com>
2014-04-04 01:22:12 -0400

    Remove check for production to mail

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,12 +1,10 @@
-if Rails.env.production?
-  ActionMailer::Base.smtp_settings = {
-    port:           '587',
-    address:        'smtp.mandrillapp.com',
-    user_name:      ENV['MANDRILL_USERNAME'],
-    password:       ENV['MANDRILL_APIKEY'],
-    domain:         'heroku.com',
-    authentication: :plain
-  }
+ActionMailer::Base.smtp_settings = {
+  port:           '587',
+  address:        'smtp.mandrillapp.com',
+  user_name:      ENV['MANDRILL_USERNAME'],
+  password:       ENV['MANDRILL_APIKEY'],
+  domain:         'heroku.com',
+  authentication: :plain
+}
 
-  ActionMailer::Base.delivery_method = :smtp
-end
+ActionMailer::Base.delivery_method = :smtp
