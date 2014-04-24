0d845daa2264db913045072e36f54a9d08fc404c
Yushun He <sonnyhe2002@gmail.com>
2014-04-23 23:32:25 -0400

    Fix error notice

--- app/models/site.rb
+++ app/models/site.rb
@@ -11,7 +11,7 @@
 
 class Site < ActiveRecord::Base
   validates :name, presence: true
-  validates :address, presence: true, uniqueness: { case_sensitive: false, message: 'The url has already been added. No need to add it twice.'}
+  validates :address, presence: true, uniqueness: { case_sensitive: false, message: ' has already been added. No need to add it twice.'}
   validate :address_is_an_url
   
   before_validation :set_name
@@ -27,7 +27,7 @@ class Site < ActiveRecord::Base
       http = Net::HTTP.new(self.address, 80)
       http.request_get('/')
     rescue SocketError => e
-      errors.add(:address, 'The url entered is not valid. Please check the url again.')
+      errors.add(:address, ' entered is not valid. Please check the url again.')
     end
   end
 end
